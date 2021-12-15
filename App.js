18ffa44e6c19e2b4251cf1a8c1e6b77f352066f0
Mounir <takimunir@gmail.com>
2021-12-15 15:15:19 +0100

    configured expo

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Provider} from 'react-redux';
+import {store} from "./store";
+
 
 export default function App() {
   return (
-      <Provider store={}>
+      <Provider store={store}>
         <View style={styles.container}>
           <Text>Hawchtaho</Text>
           <StatusBar style="auto" />
